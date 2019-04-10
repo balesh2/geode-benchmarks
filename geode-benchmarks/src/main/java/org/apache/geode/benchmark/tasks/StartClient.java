c5e3f75a8c05da244aa0dd85e421e2bd14a73c61
Helena A. Bales <hbales@pivotal.io>
2019-04-10 11:38:50 -0700

    use default idle timeout

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/StartClient.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/StartClient.java
@@ -48,7 +48,6 @@ public class StartClient implements Task {
     ClientCache clientCache = new ClientCacheFactory(GeodeProperties.clientProperties())
         .setPdxSerializer(new ReflectionBasedAutoSerializer("benchmark.geode.data.*"))
         .addPoolLocator(locator.getHostAddress(), locatorPort)
-        .setPoolIdleTimeout(-1)
         .set(ConfigurationProperties.STATISTIC_ARCHIVE_FILE, statsFile)
         .create();
 
