5208a49290c13be0471590269912aefb7459f227
Helena A. Bales <hbales@pivotal.io>
2019-05-01 11:31:46 -0700

    use lists and sets correctly

--- harness/src/main/java/org/apache/geode/perftest/infrastructure/ssh/SshInfrastructureFactory.java
+++ harness/src/main/java/org/apache/geode/perftest/infrastructure/ssh/SshInfrastructureFactory.java
@@ -17,9 +17,12 @@
 
 package org.apache.geode.perftest.infrastructure.ssh;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.geode.perftest.infrastructure.Infrastructure;
 import org.apache.geode.perftest.infrastructure.InfrastructureFactory;
@@ -30,12 +33,12 @@ public class SshInfrastructureFactory implements InfrastructureFactory {
   private final String user;
 
   public SshInfrastructureFactory(String user, String... hosts) {
-    List<String> tempHosts = Arrays.asList(hosts);
+    Set<String> tempHosts = new HashSet<>(Arrays.asList(hosts));
     String clientLocation = hosts[hosts.length-1];
     for(int i=1; i<32; i++) {
       tempHosts.add(clientLocation);
     }
-    this.hosts = tempHosts;
+    this.hosts = new ArrayList<>(tempHosts);
     this.user = user;
   }
 
