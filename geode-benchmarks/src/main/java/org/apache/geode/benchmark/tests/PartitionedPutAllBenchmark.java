a59326bc525848c6ab7b0651ef2a9c03f663993a
Helena A. Bales <hbales@pivotal.io>
2019-11-26 15:47:30 -0800

    use 1*CPU num threads

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/PartitionedPutAllBenchmark.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/PartitionedPutAllBenchmark.java
@@ -55,7 +55,7 @@ public class PartitionedPutAllBenchmark implements PerformanceTest {
   @Override
   public TestConfig configure() {
     TestConfig config = GeodeBenchmark.createConfig();
-    config.threads((int)Math.floor(Runtime.getRuntime().availableProcessors()/2));
+    config.threads(Runtime.getRuntime().availableProcessors());
     ClientServerTopology.configure(config);
     config.before(new CreatePartitionedRegion(), SERVER);
     config.before(new CreateClientProxyRegion(), CLIENT);
